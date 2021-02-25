e0e09c58bd87bc0b7d8a49fe7d272b7887dbbae5
konkovam <konkovam@example.com>
2021-02-25 17:59:51 +0300

    Использование функции input()

---
+++ python_input.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Использование функции input
+name = input("Введите Ваше имя: ")
+print("Привет, ", name)
+input("Нажмите <Enter> для закрытия окна")
