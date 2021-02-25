dc3f2c33e39d182b530ecee183acdf997105c949
konkovam <konkovam@example.com>
2021-02-25 17:10:16 +0300

    Использование метода write

---
+++ python_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Использование метода write
+
+import sys #Подключаем модуль sys
+sys.stdout.write("Строка 1\n")
+sys.stdout.write("Строка 2 ")
+sys.stdout.write("Строка 3")
