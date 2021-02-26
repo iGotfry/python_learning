d68d08644550442ca1f68fe7bdfd7549fb3a4b30
konkovam <konkovam@example.com>
2021-02-26 10:37:09 +0300

    Обработка первого исключения

---
+++ python_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#Обработка первого исключения
+try:
+    s = input("Введите данные: ")
+    print(s)
+
+except EOFError:
+    print("Обработали исключение EOFError")
